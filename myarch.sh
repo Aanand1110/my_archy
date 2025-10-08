e62239d9a96533697b70bef2397ef715c71d909c
Aanand <anandpatel4411@gmail.com>
2025-10-08 10:45:53 +0530

    changed myarch.sh

--- myarch.sh
+++ myarch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#run it in the home dir of the user after first boot
+
 #(for image viewing)
 sudo pacman -S feh 
 #(for kickstarter nvim)
@@ -7,13 +9,13 @@ sudo pacman -S git
 sudo pacman -S neovim
 #copy the nvim binary to the local destination
 sudo cp /usr/bin/nvim /usr/local/bin/
-mkdir ~/.config/nvim
+mkdir $HOME/.config/nvim
 
 # for unpacking lsps
 sudo pacman -S unzip
 
 # copy and paste the init.lua from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
-touch ~/.config/nvim/init.lua
+touch $HOME/.config/nvim/init.lua
 
 #installing firefox
 sudo pacman -S firefox
@@ -24,7 +26,7 @@ source ~/.config/envman/PATH.env
 #to update webi
 webi webi
 
-webi golang@latest
-sudo pacman -S gcc
+#webi golang@latest
+#sudo pacman -S gcc
 
 echo "everything successfull"
