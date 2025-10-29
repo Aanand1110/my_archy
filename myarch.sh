32e9a2a02c3b586baaf82004f698b30c15858e94
Aanand <anandpatel4411@gmail.com>
2025-10-29 09:52:06 +0530

    completed myarch.sh for now

--- myarch.sh
+++ myarch.sh
@@ -2,21 +2,42 @@
 
 #run it in the home dir of the user after first boot
 
-#(for kickstarter nvim)
+#for hyperland
+cd $HOME/.config/hypr
+
+curl https://raw.githubusercontent.com/aanand1110/my_archy/main/hypr/hyprland.conf > hyprland.conf
+
+cd $HOME
+
+#for basic kitty config
+
+mkdir $HOME/.config/kitty
+cd $HOME/.config/kitty
+
+curl -o current-theme.conf https://raw.githubusercontent.com/aanand1110/my_archy/main/kitty/current-theme.conf
+
+curl -o kitty.conf https://raw.githubusercontent.com/aanand1110/my_archy/main/kitty/kitty.conf
+
+curl -o kitty.conf.bak https://raw.githubusercontent.com/aanand1110/my_archy/main/kitty/kitty.conf.bak
+
+cd $HOME
+
+#installing firefox
+sudo pacman -S firefox
+
+#(for daily development workflows)
 sudo pacman -S git
-sudo pacman -S neovim
-#copy the nvim binary to the local destination
-sudo cp /usr/bin/nvim /usr/local/bin/
-mkdir $HOME/.config/nvim
 
 # for unpacking lsps
 sudo pacman -S unzip
 
-# copy and paste the init.lua from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
-touch $HOME/.config/nvim/init.lua
+sudo pacman -S neovim
+mkdir $HOME/.config/nvim
+cd $HOME/.config/nvim
 
-#installing firefox
-sudo pacman -S firefox
+curl -o init.lua https://raw.githubusercontent.com/aanand1110/my_archy/main/nvim/init.lua
+
+cd $HOME
 
 #for installing webi
 curl -sS https://webi.sh/webi | sh; \
@@ -29,6 +50,17 @@ webi webi
 
 #for waybar
 sudo pacman -S waybar
+sudo pacman -S otf-aurulent-nerd
 mkdir $HOME/.config/waybar
+cd $HOME/.config/waybar
+
+curl -o config.jsonc  https://raw.githubusercontent.com/aanand1110/my_archy/main/waybar/config.jsonc
+
+curl -o poweroff.sh https://raw.githubusercontent.com/aanand1110/my_archy/main/waybar/poweroff.sh
+
+curl -o style.css  https://raw.githubusercontent.com/aanand1110/my_archy/main/waybar/style.css
+
+cd $HOME
 
 echo "everything successfull"
+
