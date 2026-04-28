4cd6148d4a7709aabc526531327eff3d8112c555
Aanand <anandpatel4411@gmail.com>
2026-04-28 13:03:22 -0500

    made some changes to dockerandKube.sh

--- dockerKube.sh
+++ dockerKube.sh
@@ -3,5 +3,12 @@
 # run this script if you want to configure docker and K8
 
 result=""
+sudo pacman -S docker
+sudo pacman -S docker-compose
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo usermod -aG docker $USER
 
+sudo pacman -S minikube
+sudo pacman -S kubectl
 echo $result
